9935973f4e73a8ba1cc55fbd21417bb7cb03c8d8
Zhi Yi <zhiyi.yeo@u.yale-nus.edu.sg>
2020-05-21 10:18:52 +0800

    Include contextuals database in package

--- R/data.R
+++ R/data.R
@@ -32,9 +32,38 @@
 #'     One row of each actor consists of the standardized version of the name}
 #'     \item{\code{wrong}}{char Standardized version of the actor name}
 #'     \item{\code{iso}}{char 3 letter ISO codes for the country}
-#'     \item{\code{entity.type}}{char The entity type of the actor (City, Business, etc.)}
+#'     \item{\code{entity_type}}{char The entity type of the actor (City, Business, etc.)}
 #'     \item{\code{allcaps}}{char The all capital version of the standardized name}
 #'     \item{\code{caverphone - statcan}}{char Different phonetic codes of the actor names
 #'     based on different phonetic algorithms}
 #'     }
 "key_dict"
+
+#' @title Contextuals Database for subnational actors
+#' @description The contextuals database contains important contextual data for the
+#' subnational climate actors found across the different climate databases. See below for
+#' details on what data is included
+#' @format contextuals is a dataframe with contextuals information for 10462 unique actors
+#' (rows) and 15 variables (columns)
+#' #' @format \describe{
+#'     \item{\code{name}}{char Name of the subnational actor}
+#'     \item{\code{iso}}{char 3 letter ISO codes for the country of actor}
+#'     \item{\code{country}}{char Country in which actor resides in}
+#'     \item{\code{entity_type}}{char The entity type of the actor (City, Region, etc.)}
+#'     \item{\code{region}}{char The broader region which the country of the
+#'     subnational actor belongs to}
+#'     \item{\code{area}}{double Total unit area of the actor}
+#'     \item{\code{area_units}}{char Units which the area of the actor are expressed
+#'     in}
+#'     \item{\code{gdp}}{double GDP of the subnational actor}
+#'     \item{\code{initiatives_committed}}{char Climate initiatives to which the actor pledged
+#'     commitments to}
+#'     \item{\code{num_commit}}{int Number of initiatives to which actor pledged commitments
+#'     to}
+#'     \item{\code{lat}}{double Latitude of the actor}
+#'     \item{\code{lng}}{double Longitude of the actor}
+#'     \item{\code{population}}{double Total population of the actor}
+#'     \item{\code{population_year}}{int Year of recorded population}
+#'     \item{\code{state}}{char State in which the actor is situated in}
+#'     }
+"contextuals"
--- data-raw/updating_key_dicts.R
+++ data-raw/updating_key_dicts.R
@@ -4,3 +4,8 @@
 key_dict <- read.csv("key_dict_new_Apr2020_withphoneticcodes.csv",
                      encoding = "UTF-8", stringsAsFactors = F)
 save(key_dict, file = "../data/key_dict.rda")
+
+# Export contextuals database to RDA
+contextuals <- read.csv("contextuals_database_14May20_zy.csv",
+                        encoding = "UTF-8", stringsAsFactors = F)
+save(contextuals, file = "../data/contextuals.rda")
