df66233c14436239d064fe91c377a0824232ccb6
Zhi Yi <zhiyi.yeo@u.yale-nus.edu.sg>
2020-06-10 15:35:06 +0800

    Update contextuals database

--- data-raw/updating_key_dicts.R
+++ data-raw/updating_key_dicts.R
@@ -6,7 +6,7 @@ key_dict <- read.csv("key_dict_new_Apr2020_withphoneticcodes.csv",
 save(key_dict, file = "../data/key_dict.rda")
 
 # Export contextuals database to RDA
-contextuals <- read.csv("contextuals_database_3June20_zy.csv",
+contextuals <- read.csv("contextuals_database_10June20_zy.csv",
                         encoding = "UTF-8", stringsAsFactors = F)
 # save(contextuals, file = "../data/contextuals.rda")
 usethis::use_data("contextuals", overwrite = T)
