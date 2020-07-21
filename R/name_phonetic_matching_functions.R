d44958c49e144b101ea0ac17ccd4806b84eb9851
Zhi Yi <zhiyi.yeo@u.yale-nus.edu.sg>
2020-07-21 17:03:11 +0800

    Bug fixes for update key dict and vignette

--- R/name_phonetic_matching_functions.R
+++ R/name_phonetic_matching_functions.R
@@ -879,7 +879,7 @@ update_key_dict <- function(dataset, key.dict, custom_indices, unmatched_indices
   # Check for whether dataframes exist first or not
   if (exists("cust_df") & exists("unmatched_df") & exists("update_df")){
     key.dict <- rbind(key.dict, update_df, cust_df, unmatched_df)
-  } else if (exists("cust_df" & exists("update_df"))){
+  } else if (exists("cust_df") & exists("update_df")){
     key.dict <- rbind(key.dict, update_df, cust_df)
   } else if (exists("unmatched_df" & exists("update_df"))){
     key.dict <- rbind(key.dict, update_df, unmatched_df)
